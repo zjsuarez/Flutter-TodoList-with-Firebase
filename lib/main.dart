7a46c30cdedf374758862202d8987142e4ed8ca5
works <zjs.suarez@gmail.com>
2026-05-12 10:10:06 +0200

    (feat) init get it main

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todolistfirebase/src/core/di/injection.dart';
 import 'package:todolistfirebase/src/core/router/app_router.dart';
-import 'package:todolistfirebase/src/features/dashboard/domain/model/task.dart';
+import 'package:todolistfirebase/src/features/dashboard/data/model/task_model.dart';
+import 'package:todolistfirebase/src/features/dashboard/domain/entities/task.dart';
 
-void main () async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   Hive.registerAdapter(TaskAdapter());
-
+  await Hive.openBox<Task>('tasksBox');
+  configureInjection();
 
   runApp(const MainApp());
 }
